a8406eea6ea621364f71916c3ce6f602983b6cbd
Edi Zeqiri <89740646+edizeqiri@users.noreply.github.com>
2026-01-04 22:42:16 +0100

    WIP maps

--- src/world/map.rs
+++ src/world/map.rs
@@ -1,6 +1,57 @@
+use bevy::log::tracing::metadata::ParseLevelFilterError;
 use crate::world::level1;
 use bevy::prelude::*;
+use bevy_rapier2d::rapier::prelude::Point;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(level1::plugin);
+}
+
+
+struct InfiniteMap {
+    paths: Vec<InfinitePath>
+}
+impl Map for InfiniteMap {
+    fn create(&self) {
+        todo!()
+    }
+}
+struct InfinitePath;
+
+impl Vertex for InfinitePath {
+    fn create(&self) {
+        todo!()
+    }
+}
+
+impl Interpolate for InfinitePath {
+    fn interpolate(&self) {
+        todo!()
+    }
+}
+
+impl Path for InfinitePath {
+
+}
+
+trait Path: Vertex + Interpolate {
+    // vertices: Vec2, // (X1,Y1),(X2,Y2)
+    // points: Vec2
+}
+
+// Map -> 1:n Path -> Vertex
+// Points
+// 1. random linked vertices
+// 2. interpolate to smooth
+// 3. noise to path
+trait Map {
+    fn create(&self);
+
+}
+trait Vertex {
+    fn create(&self);
+}
+
+trait Interpolate {
+    fn interpolate(&self);
 }
