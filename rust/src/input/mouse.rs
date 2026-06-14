943df3efe566f3ce6394c7bbefedc986677d9c66
saschatran <transa@ethz.ch>
2026-06-14 20:27:22 +0200

    mouse click system

--- rust/src/input/mouse.rs
+++ rust/src/input/mouse.rs
@@ -1,10 +1,12 @@
 use bevy::prelude::*;
-use godot_bevy::prelude::*;
+use godot_bevy::{plugins::input::MouseButton, prelude::*};
 
 use crate::character::{Aim, player::Player};
 
 pub(crate) fn plugin(app: &mut App) {
-    app.add_systems(Update, handle_mouse_motion_system);
+    app
+        .add_systems(Update, handle_mouse_motion_system)
+        .add_systems(Update, handle_mouse_system);
 }
 
 fn handle_mouse_motion_system(
@@ -17,6 +19,23 @@ fn handle_mouse_motion_system(
     
     for event in events.read() {
         aim.vec = event.position;
-        info!("Mouse moved by: {:?}", event.position);
+    }
+}
+
+fn handle_mouse_system(
+    mut query: Query<&mut Aim, With<Player>>,
+    mut events: MessageReader<MouseButtonInput>
+) {
+    let Ok(mut aim) = query.single_mut() else {
+        return;
+    };
+    
+    for event in events.read() {
+        if event.pressed {
+            match event.button {
+                MouseButton::Left => aim.vec = event.position,
+                _ => {}
+            }
+        }
     }
 }
