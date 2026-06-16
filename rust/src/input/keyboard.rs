de49dbc7f9ae94183fa42f9acd8da5816e137f4a
Edi Zeqiri <89740646+edizeqiri@users.noreply.github.com>
2026-06-16 23:15:32 +0200

    formatting

--- rust/src/input/keyboard.rs
+++ rust/src/input/keyboard.rs
@@ -1,20 +1,15 @@
-use bevy::{math::VectorSpace, prelude::*};
-use godot::{global::Key, prelude::*};
-use godot_bevy::prelude::*;
+use bevy::prelude::*;
 use godot::classes::Input;
+use godot::global::Key;
+use godot_bevy::prelude::*;
 
 use crate::character::{Movement, player::Player};
 
 pub(crate) fn plugin(app: &mut App) {
-    app
-        .add_systems(Update, handle_keyboard_system);
+    app.add_systems(Update, handle_keyboard_system);
 }
 
-fn handle_keyboard_system(
-    mut events: MessageReader<KeyboardInput>,
-    mut query: Query<(&mut Movement), With<Player>>,
-    mut godot: GodotAccess
-) {
+fn handle_keyboard_system(mut query: Query<(&mut Movement), With<Player>>, mut godot: GodotAccess) {
     let Ok(mut movement) = query.single_mut() else {
         return;
     };
@@ -22,12 +17,18 @@ fn handle_keyboard_system(
     let gd_input = godot.singleton::<Input>();
 
     let mut base = Vec2::ZERO;
-    if gd_input.is_key_pressed(Key::W) { base += Vec2 { x: 0., y: -1. };}
-    if gd_input.is_key_pressed(Key::A) { base += Vec2 { x: -1., y: 0. };}
-    if gd_input.is_key_pressed(Key::S) { base += Vec2 { x: 0., y: 1. };}
-    if gd_input.is_key_pressed(Key::D) { base += Vec2 { x: 1., y: 0. };}
-
+    if gd_input.is_key_pressed(Key::W) {
+        base += Vec2 { x: 0., y: -1. };
+    }
+    if gd_input.is_key_pressed(Key::A) {
+        base += Vec2 { x: -1., y: 0. };
+    }
+    if gd_input.is_key_pressed(Key::S) {
+        base += Vec2 { x: 0., y: 1. };
+    }
+    if gd_input.is_key_pressed(Key::D) {
+        base += Vec2 { x: 1., y: 0. };
+    }
 
     movement.vec = base.normalize_or_zero();
-
 }
