4f900700bc1c9d60d9ea5db6f67227f09d92d643
saschatran <transa@ethz.ch>
2026-06-27 21:27:41 +0200

    score board init

--- rust/src/ui/score.rs
+++ rust/src/ui/score.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::fs::{self, File};
+use::std::io::Write;
 
 use bevy::prelude::*;
 use godot::classes::Label;
@@ -10,7 +11,7 @@ use crate::world::NameEnteredEvent;
 use crate::world::level_manager::{CurrentLevel, Score};
 
 pub(super) fn plugin(app: &mut App) {
-    app.insert_resource(load_high_score())
+    app.insert_resource(load_score_board())
         .add_systems(Update, (score_tracker, update_score_label))
         .add_observer(save_high_score);
 }
@@ -20,16 +21,36 @@ use std::path::Path;
 const HIGH_SCORE_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/resources/highscore.txt");
 const SCORE_BOARD_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/resources/score_board.csv");
 
-#[derive(Resource, Default)]
-struct HighScore {
-    count: u32
+#[derive(Resource)]
+struct ScoreBoard {
+    entry: Vec<ScoreBoardEntry>,
+    high_score: u32 // technically seen could be taken from entry directly -> but this avoids all the overhead to get it
+}
+
+impl Default for ScoreBoard {
+    fn default() -> Self {
+        Self {
+            entry: vec![
+                ScoreBoardEntry {
+                    name: "test".to_string(),
+                    score: 0,
+                },
+            ],
+            high_score: 0
+        }
+    }
+}
+
+struct ScoreBoardEntry {
+    name: String,
+    score: u32
 }
 
 fn score_tracker(
     mut death_message_reader: MessageReader<CharacterDeathMessage>,
     player_query: Query<Entity, With<Player>>,
     mut score_query: Query<&mut Score>,
-    mut high_score: ResMut<HighScore>
+    mut score_board: ResMut<ScoreBoard>
 ) {
     for message in death_message_reader.read() {
         let Ok(player) = player_query.single() else {
@@ -45,7 +66,7 @@ fn score_tracker(
         };
         
         score.count += 1;
-        high_score.count = high_score.count.max(score.count);
+        score_board.high_score = update_high_score(score.count, score_board.high_score);
     }
 }
 
@@ -54,7 +75,7 @@ fn update_score_label(
     score_query: Query<&Score, Changed<Score>>,
     current_level: Res<CurrentLevel>,
     mut scene_tree: SceneTreeRef,
-    high_score: Res<HighScore>
+    score_board: Res<ScoreBoard>
 ) {
     let level_id = current_level.level_id;
 
@@ -77,14 +98,14 @@ fn update_score_label(
     score_label.set_text(&format!(
         "Score: {}\nHigh Score: {}",
         score.count,
-        high_score.count,
+        score_board.high_score,
     ));
 }
 
 fn save_high_score(
     trigger: On<NameEnteredEvent>,
     score_query: Query<&Score>,
-    high_score: Res<HighScore>
+    mut score_board: ResMut<ScoreBoard>
 ) {
     let Ok(score) = score_query.single() else {
         info!("No score found => No high score can be saved.");
@@ -92,47 +113,80 @@ fn save_high_score(
     };
     let entered_name = &trigger.event().name;
 
-    let high_score_path = Path::new(HIGH_SCORE_PATH);
-    let current_high_score = high_score.count;
-
-    // the check is technically redundant, but avoids constant writing to file
-    if score.count >= current_high_score {
-        info!("New High Score!!!");
-        if let Err(error) = fs::write(high_score_path, score.count.to_string()) {
-            warn!(
-                "Could not write high score: {} to file {:?}",
-                error, high_score_path
-            );
-        }
-    }
-
-    save_to_score_board(entered_name, score.count);
+    score_board.high_score = update_high_score(score.count, score_board.high_score);
+    score_board.entry.push(ScoreBoardEntry {
+       name: entered_name.clone(),
+       score: score.count
+    });
+    
+    save_score_board(entered_name, score.count);
 }
 
-fn save_to_score_board(name: &String, score: u32) {
+fn update_high_score(score: u32, current_high_score: u32) -> u32 {
+    if score > current_high_score {
+        info!("NEW HIGHSCORRRRRE!!!");
+        return score;
+    };
+    return current_high_score;
+}
+
+fn save_score_board(
+    name: &str,
+    score: u32,
+) {
     let score_board_path = Path::new(SCORE_BOARD_PATH);
-    if let Err(error) = fs::write(score_board_path, format!("{},{}", name, score)) {
-        warn!(
-            "Could not write score to scoreboard: {} to file {:?}",
-            error, score_board_path
-        );
+
+    let Ok(mut file) = File::options()
+        .append(true)
+        .create(true)
+        .open(score_board_path)
+    else {
+        error!("No Score Board File could be opened at: {:?}", SCORE_BOARD_PATH);
+        return;
+    };
+
+    if let Err(error) = writeln!(file, "{},{}", name, score) {
+        error!("Could not write to score board: {}", error);
     }
 }
 
-fn load_high_score() -> HighScore {
-    let Ok(value) = fs::read_to_string(HIGH_SCORE_PATH) else {
-        warn!("Could not read high score from file: {}", HIGH_SCORE_PATH);
-        return HighScore { count: 0 };
+fn load_score_board() -> ScoreBoard {
+    let Ok(score_board_file) = fs::read_to_string(SCORE_BOARD_PATH) else {
+        warn!("Could not read high score from file: {}", SCORE_BOARD_PATH);
+        return ScoreBoard {
+            high_score: 0,
+            entry: Vec::default() };
     };
 
-    let Ok(count) = value.trim().parse::<u32>() else {
-        warn!(
-            "Could not parse high score from file: {}. Value was: {:?}",
-            HIGH_SCORE_PATH,
-            value.trim()
-        );
-        return HighScore { count: 0 };
-    };
-
-    HighScore { count }
+    let mut score_board: Vec<ScoreBoardEntry> = score_board_file
+        .lines()
+        .filter_map(|line| {
+            let Some((name, score_as_string)) = line.split_once(',') else {
+                error!("Cannot read score board at: {}", SCORE_BOARD_PATH);
+                return None;
+            };
+    
+            let Ok(score) = score_as_string.trim().parse::<u32>() else {
+                error!("Cannot parse score in {}", SCORE_BOARD_PATH);
+                return None;
+            };
+    
+            Some(ScoreBoardEntry {
+                name: name.trim().to_string(),
+                score,
+            })
+        })
+        .collect();
+    
+    score_board.sort_by(|a, b| b.score.cmp(&a.score));
+    
+    let high_score = score_board
+        .first()
+        .map(|entry| entry.score)
+        .unwrap_or(0);
+    
+    ScoreBoard {
+        entry: score_board,
+        high_score: high_score,
+    }
 }
