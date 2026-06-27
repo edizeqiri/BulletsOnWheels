cdcc009865342b9bee290afe1a5fb4c0667a4fb1
saschatran <transa@ethz.ch>
2026-06-27 14:02:56 +0200

    read in signal

--- rust/src/ui/defeat.rs
+++ rust/src/ui/defeat.rs
@@ -1,12 +1,19 @@
-use bevy::prelude::*;
+use bevy::{ecs::event::Trigger, prelude::*};
 use bevy_asset_loader::asset_collection::AssetCollection;
+use godot::classes::LineEdit;
 use godot_bevy::prelude::*;
 
 use crate::gamestate::InGameState;
 
 pub(super) fn plugin(app: &mut App) {
-    app
-    .add_systems(OnEnter(InGameState::DEFEAT), ask_for_player_name_system);
+    app.add_plugins(GodotSignalsPlugin::<NameEnteredEvent>::default())
+        .add_systems(OnEnter(InGameState::DEFEAT), spawn_ask_for_player_name_system)
+        .add_observer(name_submitted);
+}
+
+#[derive(Event, Clone)]
+struct NameEnteredEvent {
+    name: String
 }
 
 #[derive(AssetCollection, Resource)]
@@ -15,11 +22,37 @@ pub(crate) struct DefeatAssets {
     pub enter_name_scene: Handle<GodotResource>,
 }
 
-fn ask_for_player_name_system(
+fn spawn_ask_for_player_name_system(
+    text_field_object: Query<&GodotNodeHandle, With<LineEditMarker>>,
+    entered_name_signal: GodotSignals<NameEnteredEvent>,
     mut commands: Commands,
     assets: Res<DefeatAssets>,
 ) {
+    let Ok(handler) = text_field_object.single() else {
+        return;
+    };
+    
+    entered_name_signal.connect(
+        *handler, 
+        LineEditSignals::TEXT_SUBMITTED, 
+        None, 
+        |args, _node_handle, _ent| {
+            let Some(name) = args.get(0)?.try_to::<String>().ok() else {
+                error!("Name could not be parsed");
+                return None;
+            };
+        
+            Some(NameEnteredEvent { name })
+        }
+    );
+    
     commands
         .spawn_empty()
         .insert(GodotScene::from_handle(assets.enter_name_scene.clone()));
 }
+
+fn name_submitted(trigger: On<NameEnteredEvent>) {
+    let entered_name = &trigger.event().name;
+    info!("entered name {}", entered_name)
+}
+
