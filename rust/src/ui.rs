d9d2c27ed9240855b01bb498034eab862971927d
saschatran <transa@ethz.ch>
2026-07-06 17:34:23 +0200

    tmp

--- rust/src/ui.rs
+++ rust/src/ui.rs
@@ -7,9 +7,9 @@ pub(super) fn plugin(app: &mut App) {
     app.add_observer(on_mouse_enter_button_animation);
 }
 
-/*fn on_mouse_enter_button_animation(
+fn on_mouse_enter_button_animation(
     trigger: On<ButtonEnteredEvent>,
     godot: GodotAccess
 ) {
     let nine_patct_rect = trigger.button_handle.get_
-    */
+}
